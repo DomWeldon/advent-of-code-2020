63c8ede696b43681c9a0909c4103bed4e223c194
Dom Weldon <dom.weldon@decisionlab.co.uk>
2020-12-01 22:24:30 +0000

    fixed python unittest

--- python/01/challenge/test_expenses.py
+++ python/01/challenge/test_expenses.py
@@ -1,3 +1,5 @@
+import math
+
 from . import expenses
 
 
@@ -11,15 +13,15 @@ def test_find_inputs_which_sum_to():
         675,
         1_456,
     ]
-    expected_product = 514579
-    expectd_sum = 2020
+    expected_product = 514_579
+    expected_sum = 2_020
 
     # act
     actual_nums = expenses.find_inputs_which_sum_to(
-        fixture_expenses, expectd_sum, 2
+        fixture_expenses, expected_sum, 2
     )
-    actual_product = actual_nums[0] * actual_nums[1]
+    actual_product = math.prod(actual_nums)
 
     # assert
-    assert actual_nums == {1721, 299}
+    assert actual_nums == {1_721, 299}
     assert actual_product == expected_product
